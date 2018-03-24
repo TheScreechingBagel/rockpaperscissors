46c88b713faae860705eca3354aa1344f1c1b245
tihbab <tihonbabchynskyy@gmail.com>
2018-03-24 14:08:26 -0700

    FIXES: variable more readable, possible input options for user.

--- game.py
+++ game.py
@@ -7,7 +7,7 @@ paper = "бумага"
 x = "да"
 while x == "да" or x == "y":
 
-    plrin = input("что загадываешь? Камень ножницы или бумагу? ")
+    player_input = input("что загадываешь? Камень ножницы или бумагу? or [r/p/s] ")
 
     print('Rock, paper, scissors!')
 
@@ -15,25 +15,25 @@ while x == "да" or x == "y":
     print(ran)
 
 
-    if plrin == rock or plrin == "r": 
+    if player_input == rock or player_input == "r": 
         if ran == rock:
             print("Draw!")
         elif ran == scissors:
             print("I've lost!")
         elif ran == paper:
             print("You've lost!")
-    if plrin == scissors or plrin == "s": 
+    if player_input == scissors or player_input == "s": 
         if ran == rock:
             print("You've lost!")
         elif ran == scissors:
             print("Draw!")
         elif ran == paper:
             print("I've lost!")
-    if plrin == paper or plrin == "p":
+    if player_input == paper or player_input == "p":
         if ran == rock:
             print("I've lost!")
         elif ran == scissors:
             print("You've lost!")
         elif ran == paper:
             print("Draw!")
-    x = input("Ещё раз? ")
+    x = input("Ещё раз? [да/нет] or [y/n] ") 
